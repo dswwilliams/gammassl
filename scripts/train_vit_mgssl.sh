f460042f7558cbd95ca2cdf95ef811d63dab00ed
David Williams <dw@robots.ox.ac.uk>
2024-02-02 12:13:37 +0000

    new trainin script, cds into scripts first

--- scripts/train_vit_mgssl.sh
+++ scripts/train_vit_mgssl.sh
@@ -5,12 +5,14 @@ echo "Getting the script directory..."
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Script directory: $script_dir"
 
-# Define the path to the train.py script relative to the script's directory
-train_py_path="$script_dir/../training/train.py"
+# cd into the script's directory
+cd "$script_dir"
 
-# Construct the Python script execution command
-python_command="python3 \"$train_py_path\" --model_arch vit_m2f --use_proto_seg False --frozen_target True --mask_input True $*"
+train_py_path="$../training/train.py"
 
-# Announce and run the Python script
-echo "Executing: $python_command"
-eval "$python_command"
+python3 "$train_py_path" \
+                --model_arch vit_m2f \
+                --use_proto_seg False \
+                --frozen_target True \
+                --mask_input True \
+                $*
