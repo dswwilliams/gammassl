36fd03bf3179f6300a76abaa7885470a34932f10
David Williams <dw@robots.ox.ac.uk>
2024-02-02 11:54:17 +0000

    new training script

--- scripts/train_vit_mgssl.sh
+++ scripts/train_vit_mgssl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Get the absolute path to the directory where the script is located
-dir_command="cd \"$( dirname \"${BASH_SOURCE[0]}\" )\" &> /dev/null && pwd"
-echo "Executing: $dir_command"
-script_dir=$(eval $dir_command)
+echo "Getting the script directory..."
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Script directory: $script_dir"
 
 # Define the path to the train.py script relative to the script's directory
 train_py_path="$script_dir/../training/train.py"
@@ -13,4 +13,4 @@ python_command="python3 \"$train_py_path\" --model_arch vit_m2f --use_proto_seg 
 
 # Announce and run the Python script
 echo "Executing: $python_command"
-eval $python_command
+eval "$python_command"
