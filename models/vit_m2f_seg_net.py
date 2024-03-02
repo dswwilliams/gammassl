7b21b888fb1ca28326239378993afb475906f1d9
David Williams <dw@robots.ox.ac.uk>
2024-03-02 17:25:06 +0000

    changes to vit m2f seg net

--- models/vit_m2f_seg_net.py
+++ models/vit_m2f_seg_net.py
@@ -1,19 +1,37 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from tqdm import tqdm
-import numpy as np
-import pickle
 import sys
-import copy
-# sys.path.append("/Users/dw/code/pytorch/dinov2/dinov2")
-# sys.path.append("/Users/dw/code/pytorch/dinov2")
 sys.path.append("../")
 from models.get_dino_from_repo import get_repo_dino
 from models.base_seg_net import BaseSegNet
 from utils.m2f_utils import semantic_inference
+from models.setr_decode_head import SETRUPHead_M2F
+from models.mask2former import MultiScaleMaskedTransformerDecoder
+
+class ViT_M2F_Decoder(nn.Module):
+    """
+    Pytorch module for the decoder of the ViT_M2F_SegNet.
+    Applies a progressively upsampling decoder, followed by a transformer decoder.
+    """
+    def __init__(self, decoder_pup, transformer_decoder):
+        super().__init__()
+        self.decoder_pup = decoder_pup
+        self.transformer_decoder = transformer_decoder
+    def forward(self, shallow_features):
+
+        deep_features_list = self.decoder_pup(shallow_features)
+        output = self.transformer_decoder(x=deep_features_list[:-1], mask_features=deep_features_list[-1])
+
+        return output
+
+
 
 class ViT_M2F_SegNet(BaseSegNet):
+    """
+    Pytorch module for a segmentation network that uses Vision Transformer (ViT) as the encoder and Mask2Former as the decoder.
+    Based on: https://github.com/facebookresearch/Mask2Former
+    """
     def __init__(self, device, opt, num_known_classes):
         super().__init__(device, opt, num_known_classes)
         self.opt = opt
@@ -21,52 +39,38 @@ class ViT_M2F_SegNet(BaseSegNet):
         self.intermediate_dim = self.opt.intermediate_dim
         self.prototype_len = self.opt.prototype_len
 
-
-        ################################################################################################
-        ### defining encoder ###
+        # defining encoder
         vit_dino = get_repo_dino(dino_path=self.opt.dino_path, lora_rank=self.opt.lora_rank, vit_size=self.opt.vit_size)
         self.encoder = vit_dino
         self.encoder_dim = vit_dino.embed_dim
-        ################################################################################################
 
-        ################################################################################################
-        ### choosing decoder ###
-        from models.setr_decode_head import SETRUPHead_M2F
+
+        # decoder_pup Progressively UPsamples the features
         decoder_pup = SETRUPHead_M2F(
                                 global_scales=[0.5, 1, 2, 4],
                                 in_channels=self.encoder_dim,
                                 out_channels=self.intermediate_dim,
                                 )
-        
-        from models.mask2former import MultiScaleMaskedTransformerDecoder
+
+        # transformer_decoder refines the initial query features conditioned on the pup features
         transformer_decoder = MultiScaleMaskedTransformerDecoder(
                                                         in_channels=256,
                                                         mask_classification=True,       # has to be True
                                                         num_classes=19,                 # affects the linear layer producing the outputs_class
                                                         hidden_dim=256,                 # dimensionality of features throughout the transformer decoder (self-attention, cross-attention, mlp etc.)
                                                         num_queries=19,                 # number of query features
-                                                        nheads=self.opt.nheads,                       # number of heads for self-attention and cross-attention
-                                                        dim_feedforward=256,           # the internal dimensionality of the FFN (in FFN: hidden_dim -> dim_feedforward -> hidden_dim with Linear layers)
+                                                        nheads=self.opt.nheads,         # number of heads for self-attention and cross-attention
+                                                        dim_feedforward=256,            # the internal dimensionality of the FFN (in FFN: hidden_dim -> dim_feedforward -> hidden_dim with Linear layers)
                                                         dec_layers=3,                   # number of rounds of [self-attn, cross-attn, FFN] to do
                                                         pre_norm=True,                  # whether not do layer norm at start or end of attn and FFN layers
                                                         mask_dim=256,                   # dim of mask embed, which needs to the same as the dim of the mask features
                                                         enforce_input_project=False,
                                                         )
 
-        class Decoder(nn.Module):
-            def __init__(self,):
-                super().__init__()
-                self.decoder_pup = decoder_pup
-                self.transformer_decoder = transformer_decoder
-            def forward():
-                pass
+        # defining overall decoder
+        self.decoder = ViT_M2F_Decoder(decoder_pup, transformer_decoder)
 
-        self.decoder = Decoder()
-
-        ################################################################################################
-
-        ################################################################################################
-        ### define projection network ###
+        # defining projection network
         nonproj_len = self.intermediate_dim if self.opt.use_deep_features else self.encoder_dim
         from models.projection_networks import ProjectionMLP
         self.projection_net = ProjectionMLP(
@@ -74,25 +78,22 @@ class ViT_M2F_SegNet(BaseSegNet):
                                 output_feature_len=self.prototype_len, 
                                 dropout_prob=None,
                                 ).to(self.device)
-        ################################################################################################
 
-        ################################################################################################
-        ### to device ###
-        self.to_device()
-        ################################################################################################
+        # putting model on device
+        self.to(self.device)
 
 
     def extract_features(self, x: torch.Tensor, use_deep_features: bool = False, masks: torch.Tensor = None) -> torch.Tensor:
         """ 
-        extract features from encoder 
-        input: x.shape = (batch_size, 3, H, W)
+        Extract high-dim features describing the input image x using the encoder, and optionally the decoder.
 
-        if use_deep_features is True or self.opt.use_deep_features is True:
-        output: features.shape = [batch_size, (H//patch_size)*(W//patch_size), encoder_dim]
+        Args: 
+            x, image of shape = [batch_size, 3, H, W]
 
-        else:
-        output: features.shape = [batch_size, (H//patch_size)*(W//patch_size), intermediate_dim]
-    
+        Returns:
+            features, high-dim features of shape = [batch_size, (H//patch_size)*(W//patch_size), encoder_dim]
+             if use_deep_features is True or self.opt.use_deep_features is True
+             else, shape = [batch_size, (H//patch_size)*(W//patch_size), intermediate_dim]    
         """
         H, W = x.shape[-2:]
 
@@ -110,14 +111,27 @@ class ViT_M2F_SegNet(BaseSegNet):
             shallow_features = shallow_features.reshape(bs, C, H//self.encoder.patch_size, W//self.encoder.patch_size)
 
         if (self.opt.use_deep_features) or (use_deep_features):
-            deep_features = self.decode_features(shallow_features, return_list=False)
+            deep_features = self.decoder.decoder_pup(shallow_features)[-1]
+            
+
             return deep_features
         else:
             return shallow_features
         
-    def extract_m2f_output(self, x: torch.Tensor, masks: torch.Tensor = None) -> torch.Tensor:
+    def extract_m2f_output(self, x: torch.Tensor, masks: torch.Tensor = None) -> dict[str, torch.Tensor]:
         """
-        - should be similar to target branch rather than query branch
+        Extracts the output of the Mask2Former network, given the input image x and optionally the masks.
+        The m2f_output is a dictionary containing the predicted logits and masks, which can be combined to get the final segmentation masks.
+
+        Args:
+            x: input image of shape = [batch_size, 3, H, W]
+
+        Returns:
+            m2f_outout, dictionary of the form:
+                m2f_output = {
+                    "pred_logits": torch.Tensor, shape = [batch_size, num_classes, num_queries+1]   (see definition of transformer_decoder)
+                    "pred_masks": torch.Tensor, shape = [batch_size, num_classes, H, W]
+                }
         """
 
         H, W = x.shape[-2:]
@@ -134,32 +148,37 @@ class ViT_M2F_SegNet(BaseSegNet):
             shallow_features = shallow_features.reshape(bs, C, H//self.encoder.patch_size, W//self.encoder.patch_size)
 
 
-        # do this with the target branch, extract_m2f_output() is used in labelled task
-        deep_features_list = self.decode_features(shallow_features, return_list=True)
+        m2f_output = self.decoder(shallow_features)    
+        m2f_output["pred_masks"] = F.interpolate(m2f_output["pred_masks"], size=(H, W), mode="bilinear", align_corners=False)
 
-        output = self.decoder.transformer_decoder(x=deep_features_list[:-1], mask_features=deep_features_list[-1])
-        output["pred_masks"] = F.interpolate(output["pred_masks"], size=(H, W), mode="bilinear", align_corners=False)
-        return output
+        return m2f_output
 
-    
-    def decode_features(self, x, return_list=True):
+    def extract_proj_features(self, x: torch.Tensor, masks: torch.Tensor = None) -> torch.Tensor:
         """
-        decodes features from encoder
-        input: x.shape = [batch_size, (H//patch_size)*(W//patch_size)+1, encoder_dim]
+        Extracts features from x, then projects them using the projection network.
+
+        Args:
+            x: input image of shape = [batch_size, 3, H, W]
+
+        Returns:
+            proj_features: projected features of shape = [batch_size, prototype_len, (H//patch_size), (W//patch_size)]
         """
-
-        if return_list:
-            return self.decoder.decoder_pup(x)
-        else:
-            return self.decoder.decoder_pup(x)[-1]
-
-    def extract_proj_features(self, x, masks=None): 
-        """ extract projected features """
         features = self.extract_features(x, masks=masks)
         proj_features = self.projection_net(features)
         return proj_features
     
-    def get_seg_masks(self, x, high_res=False, masks=None, return_mask_features=False):
+    def get_seg_masks(self, x: torch.Tensor, high_res=False, masks: torch.Tensor = None) -> torch.Tensor:
+        """
+        Get segmentation masks from the input image x.
+
+        Args:
+            x: input image of shape = [batch_size, 3, H, W]
+
+        Returns:
+            seg_masks: segmentation masks of shape = [batch_size, num_classes, H, W] if high_res is True
+                else, seg_masks.shape = [batch_size, num_classes, (H//patch_size), (W//patch_size)] if high_res is False
+
+        """
         H, W = x.shape[-2:]
         # we want grads if lora rank is not None or if train_vit is True
         if (self.opt.lora_rank is not None) or (self.opt.train_vit):
@@ -172,22 +191,17 @@ class ViT_M2F_SegNet(BaseSegNet):
             bs, _, C = shallow_features.shape
             shallow_features = shallow_features.permute(0, 2, 1)      # [bs, C, N]
             shallow_features = shallow_features.reshape(bs, C, H//self.encoder.patch_size, W//self.encoder.patch_size)
+            
+        m2f_output = self.decoder(shallow_features)
 
-
-        deep_features_list = self.decode_features(shallow_features, return_list=True)
-
-        output = self.decoder.transformer_decoder(x=deep_features_list[:-1], mask_features=deep_features_list[-1])
         if high_res:
-            output["pred_masks"] = F.interpolate(output["pred_masks"], size=(H, W), mode="bilinear", align_corners=False)
+            m2f_output["pred_masks"] = F.interpolate(m2f_output["pred_masks"], size=(H, W), mode="bilinear", align_corners=False)
 
-        seg_masks = semantic_inference(output["pred_logits"], output["pred_masks"])
+        seg_masks = semantic_inference(m2f_output["pred_logits"], m2f_output["pred_masks"])
 
-        if return_mask_features:
-            return seg_masks, deep_features_list[-1]
-        else:
-            return seg_masks
+        return seg_masks
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.get_seg_masks(x, include_void=False, high_res=True)
 
 
@@ -223,27 +237,5 @@ if __name__ == "__main__":
     print("pred_masks", output["pred_masks"].shape)
 
 
-    seg_masks = seg_net.get_seg_masks(x, include_void=False, high_res=True)
+    seg_masks = seg_net.get_seg_masks(x, high_res=True)
     print(seg_masks.shape)
-
-
-    # for f in output:
-    #     print(f.shape)
-
-    # output = seg_net.transformer_decoder(x=output[:-1], mask_features=output[-1])
-    
-    # print("pred_logits", output["pred_logits"].shape)
-    # print("pred_masks", output["pred_masks"].shape)
-
-    # x = torch.randn(1, 3, 224, 224)
-    # output = seg_net.extract_features(x)
-    # print(output.shape)
-    # output = seg_net.extract_proj_features(x)
-    # print(output.shape)
-    # output = seg_net.get_seg_masks(x, include_void=False, high_res=True)
-    # print(output.shape)
-
-
-    # for key in seg_net.state_dict().keys():
-    #     if "lora" in key:
-    #         print(key)
